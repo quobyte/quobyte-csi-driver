1236dc7cd19e4570de591a272fd06f792bf75013
Venkata Subbarao Chunduri <venkat@quobyte.com>
2026-08-18 19:01:45 +0200

    Fix release script

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,8 @@
-
 #!/bin/bash
 
 DEFAULT_CONTAINER_URL_BASE="quay.io/quobyte/csi"
-# change default with with CONTAINER_URL_BASE="<container-base-url>" ./build
+# change default with with CONTAINER_URL_BASE="<container-base-url>" ./build.sh
 CONTAINER_URL_BASE="${CONTAINER_URL_BASE:-$DEFAULT_CONTAINER_URL_BASE}"
-# https://helm.sh/docs/topics/chart_repository/#github-pages-example
-# Quobyte CSI charts are hosted as github pages. Artifacthub.io uses this
-# location to grab the deployable charts from docs/index.yaml
 
 # buildx needs "load" for local testing (vs push to remote)
 LOCAL_IMAGE="${LOCAL_IMAGE:-false}"
@@ -16,9 +12,6 @@ if [[ "$(dirname $0)" != '.' ]]; then
   echo "Executing build command in $(dirname $0)"
   cd "$(dirname $0)"
 fi
-
-CHART_PACKAGE_DIR="../helm" 
-CHART_DIR="../csi-driver-templates"
 
 # arg1 - command exit status, arg2 - error message to be printed
 exit_if_failure() {
@@ -38,14 +31,10 @@ validate_version() {
   if [[ ${DISABLE_VERSION_VALIDATION} = 'true' ]]; then
     return
   fi
-  VERSION=$1
-  if [[ -z "${VERSION}" || "{$VERSION}" == *\ * ]]; then
-    echo "FAILURE: ${VERSION} is not a valid version string. Version must not be empty or should not contain any spaces"
-    exit 1
-  fi
-  # make sure version starts with v and has exactly 6 chars (vX.Y.Z)
-  if [[ "$VERSION" != v.* && "${#VERSION}" -ne 6 ]]; then
-    echo "version must start be of the form vX.Y.Z (ex, v1.8.3)"
+  VERSION="$1"
+  # must be of the form vX.Y.Z (ex, v1.8.3) -- any number of digits per component
+  if [[ ! "${VERSION}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "FAILURE: '${VERSION}' is not a valid version string. Version must be of the form vX.Y.Z (ex, v1.8.3)"
     exit 1
   fi
 }
@@ -53,6 +42,7 @@ validate_version() {
 container_build_and_push(){
   if [[ -z "${CONTAINER_URL_BASE}" ]]; then
     echo "FAILURE: container base url should not be empty"
+    exit 1
   fi
   validate_version $1
   VERSION=$1
@@ -70,21 +60,57 @@ container_build_and_push(){
   fi
 }
 
+# Tags HEAD with the release version and pushes just that tag to origin -- not "--tags" (which
+# would push every local tag) and not the branch itself
+create_and_push_release_tag(){
+  local version="$1"
+  validate_version "${version}"
+
+  if [[ "$(git rev-parse --abbrev-ref HEAD)" != "master" ]]; then
+    echo 'FAILURE: release can only be made on the master branch'
+    exit 1
+  fi
+  if [[ -n "$(git status --porcelain)" ]]; then
+    echo 'FAILURE: requires clean directory (no staged/unstaged/untracked files) in repo'
+    exit 1
+  fi
+
+  git pull
+  exit_if_failure "$?" 'Cannot pull latest master. Fix the reported issue and retry.'
+
+  if [[ -n "$(git tag -l "${version}")" ]]; then
+    echo "FAILURE: release version tag ${version} already exists on the local repo."
+    exit 1
+  fi
+  if [[ -n "$(git ls-remote --tags origin "${version}")" ]]; then
+    echo "FAILURE: release version tag ${version} already exists on the remote origin."
+    exit 1
+  fi
+
+  git tag "${version}"
+  exit_if_failure "$?" "Cannot tag release version ${version}. Fix the reported issue \
+(you may need to run 'git tag -d ${version}' to undo it)."
+
+  git push origin "${version}"
+  exit_if_failure "$?" "Cannot push release tag ${version} to origin. Fix the reported issue \
+(you may need to run 'git tag -d ${version}' and 'git push --delete origin ${version}' to undo it)."
+}
+
 print_post_release_instructions(){
+  local version="$1"
   echo ''
   echo ''
   echo -e '\e[33mPlease go to https://github.com/quobyte/quobyte-csi/releases'
-  echo -e "and make a release for the tag version ${VERSION} with release notes\e[0m"
+  echo -e "and make a release for the tag version ${version} with release notes\e[0m"
 }
 
 if [[ "$1" = '-h' || "$1" = '--help' ]]; then
-  echo './build                                                 Builds the executable'
-  echo './build container <release-tag>"                        Builds pre and pushes container'
-  echo './build release "<release-tag>" "<chart-version>"       Builds the executable, docker image and'
-  echo '                                         pushes the container and creates a helm chart'
-  echo '                                         for the release'
-  echo "Example: ./build [container] v0.2.0"
-  echo "         ./build release v1.8.4 1.9.4"
+  echo './build.sh                                 Builds the executable'
+  echo './build.sh container <release-tag>         Builds and pushes container'
+  echo './build.sh release <release-tag>           Builds and pushes container, then creates'
+  echo '                                            and pushes the git release tag'
+  echo "Example: ./build.sh [container] v0.2.0"
+  echo '         ./build.sh release v1.8.4'
   exit 0
 else
   echo 'Building executable'
@@ -105,28 +131,10 @@ else
   fi
 
   if [[ "$1" == "container" ]]; then
-    container_build_and_push $2
+    container_build_and_push "$2"
   elif [[ "$1" == "release" ]]; then
-    if [[ $(git rev-parse --abbrev-ref HEAD) != "master" ]]; then
-      echo 'FAILURE: release can only be made on master branch'
-      exit 1
-    fi
-    if [[ ! -z "$(git status --porcelain)" ]]; then
-      echo 'Requires clean directory (no stage/unstaged/untracked files) in repo'
-      exit 1
-    fi
-    git pull
-    if [[ ! -z "$(git tag -l ${VERSION})" ]]; then
-      exit_if_failure "1" "Release version tag already exists on local repo."
-    fi
-    container_build_and_push $2
-    if [[ ! -z "$(git ls-remote --tags origin ${VERSION})" ]]; then
-      exit_if_failure "1" "Release version tag already exists on the remote origin."
-    fi
-    git tag "${VERSION}"
-    exit_if_failure "$1" "Cannot tag release version. Fix the reported issue (you may need to reset head to undo "Release commit")"
-    git push origin master --tags
-    print_post_release_instructions
+    container_build_and_push "$2"
+    create_and_push_release_tag "$2"
+    print_post_release_instructions "$2"
   fi
 fi
-
